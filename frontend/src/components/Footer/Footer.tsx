748e6bb59fea6d715a0ce0fd33955554fb65ad22
Lucas de Lima Spinosa <lucasspinosa.software@gmail.com>
2026-09-14 15:36:25 -0300

    Criação do Footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,11 +1,75 @@
-import React from 'react'
+import React from "react";
+
+import LinkedInIcon from "../../assets/linkedin.svg";
+import GitHubIcon from "../../assets/github.svg";
+import GmailIcon from "../../assets/gmail.svg";
 
 const Footer = () => {
-  return (
-    <div>
-      zzzzzzzzzzzzzzzzz
-    </div>
-  )
-}
+  const currentYear = new Date().getFullYear();
 
-export default Footer
+  return (
+    <footer
+      className="
+      py-3 
+      border-top
+    "
+    >
+      <div
+        className="  
+        d-flex 
+        align-items-center
+        flex-column
+        flex-md-row
+      "
+      >
+        <p
+          className="
+          col-md-3 
+          mb-0 
+          text-body-secondary
+          mx-auto
+        "
+        >
+          © {currentYear} Lucas de Lima Spinosa dos Santos
+        </p>
+        <ul
+          className="
+          d-flex
+          nav
+          align-items-center
+        "
+        >
+          <li className="nav-item">
+            <a
+              href="https://www.linkedin.com/in/lucas-spinosa-software/"
+              target="_blank"
+              className="nav-link"
+            >
+              <img src={LinkedInIcon} alt="LinkedIn Icon" width={40} />
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="https://github.com/LucasSpinosa"
+              target="_blank"
+              className="nav-link"
+            >
+              <img src={GitHubIcon} alt="GitHub Icon" width={30} />
+            </a>
+          </li>
+          <li className="nav-item me-md-2">
+            <a
+              href="mailto:lucasspinosa.software@gmail.com"
+              target="_blank"
+              className="nav-link"
+            >
+              <img src={GmailIcon} alt="Gmail Icon" width={40} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
